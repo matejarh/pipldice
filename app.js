5ff1ee960ffd53688760531f687348ac3a2ea0e7
matejarh <matej.arh@gmail.com>
2017-08-03 00:05:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2944,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Pipldice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LcdhysUAAAAAJk6_TrBjeEG7PkYN_ZIttQe0lse',  // <----- EDIT ME!
-  redirect_uri: 'https://pipldice.github.io',
+  recaptcha_sitekey: '6Ld_gisUAAAAABu_Cea4q1JD6mMuTQlWRZBjplbV',  // <----- EDIT ME!
+  redirect_uri: 'https://matejarh.github.io/pipldice/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
